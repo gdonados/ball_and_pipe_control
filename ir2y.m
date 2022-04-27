f5d083747378888af51a91c4ae94180ac5b4dfaa
gdonados <79154150+gdonados@users.noreply.github.com>
2022-04-27 14:23:36 -0400

    Update ir2y.m

--- ir2y.m
+++ ir2y.m
@@ -15,11 +15,21 @@ function [y, pipe_percentage] = ir2y(ir)
  ir_top    = 55; % "                        " top of pipe
  y_top     = 0.9144; % Ball at top of the pipe [m]
  height_ir = ir_bottom - ir_top; %Height of pipe in IR readings
+ ir_reading = 0; %Variable to clip IR input if out of bounds
 
 %% Bound the IR reading and send error message 
 % (remeber the IR values are inverted ie small values == large height and large values == small height)
+if(ir < ir_top)
+ ir_reading = ir_top;
+ disp(Error: Ball reading too high);
+elseif (ir > ir_bottom)
+ ir_reading = ir_bottom;
+ disp(Error: Ball reading too low);
+else
+ ir_reading = ir;
+end
 
 %% Set
- pipe_percentage = ir/height_ir;
- y = 1 - y_top*pipe_percentage;
+ pipe_percentage = ir_reading/height_ir; % Convert to a percentage of total pipe height
+ y = 1 - y_top*pipe_percentage;  % Convert to hieght from bottom of pipe, instead of from top
 
